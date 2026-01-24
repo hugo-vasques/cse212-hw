aed4436ee2e4beb615574d7c3ab4a2bf5bb8218c
MaCK34 <prom23hugomarcelo.sa@gmail.com>
2026-01-24 15:48:35 -0500

    Implement maze movement logic with directional checks

--- week03/code/Maze.cs
+++ week03/code/Maze.cs
@@ -16,50 +16,83 @@
 /// </summary>
 public class Maze
 {
-    private readonly Dictionary<ValueTuple<int, int>, bool[]> _mazeMap;
+    private readonly Dictionary<(int, int), bool[]> _mazeMap;
     private int _currX = 1;
     private int _currY = 1;
 
-    public Maze(Dictionary<ValueTuple<int, int>, bool[]> mazeMap)
+    public Maze(Dictionary<(int, int), bool[]> mazeMap)
     {
         _mazeMap = mazeMap;
     }
 
-    // TODO Problem 4 - ADD YOUR CODE HERE
     /// <summary>
-    /// Check to see if you can move left.  If you can, then move.  If you
-    /// can't move, throw an InvalidOperationException with the message "Can't go that way!".
+    /// Check if moving left is allowed. If it is, move left.
+    /// Otherwise, throw an exception.
     /// </summary>
     public void MoveLeft()
     {
-        // FILL IN CODE
+        // Index 0 represents the Left direction
+        if (_mazeMap.ContainsKey((_currX, _currY)) && _mazeMap[(_currX, _currY)][0])
+        {
+            _currX--;
+        }
+        else
+        {
+            throw new InvalidOperationException("Can't go that way!");
+        }
     }
 
     /// <summary>
-    /// Check to see if you can move right.  If you can, then move.  If you
-    /// can't move, throw an InvalidOperationException with the message "Can't go that way!".
+    /// Check if moving right is allowed. If it is, move right.
+    /// Otherwise, throw an exception.
     /// </summary>
     public void MoveRight()
     {
-        // FILL IN CODE
+        // Index 1 represents the Right direction
+        if (_mazeMap.ContainsKey((_currX, _currY)) && _mazeMap[(_currX, _currY)][1])
+        {
+            _currX++;
+        }
+        else
+        {
+            throw new InvalidOperationException("Can't go that way!");
+        }
     }
 
     /// <summary>
-    /// Check to see if you can move up.  If you can, then move.  If you
-    /// can't move, throw an InvalidOperationException with the message "Can't go that way!".
+    /// Check if moving up is allowed. If it is, move up.
+    /// Otherwise, throw an exception.
     /// </summary>
     public void MoveUp()
     {
-        // FILL IN CODE
+        // Index 2 represents the Up direction
+        // Reminder: moving up usually decreases Y
+        if (_mazeMap.ContainsKey((_currX, _currY)) && _mazeMap[(_currX, _currY)][2])
+        {
+            _currY--;
+        }
+        else
+        {
+            throw new InvalidOperationException("Can't go that way!");
+        }
     }
 
     /// <summary>
-    /// Check to see if you can move down.  If you can, then move.  If you
-    /// can't move, throw an InvalidOperationException with the message "Can't go that way!".
+    /// Check if moving down is allowed. If it is, move down.
+    /// Otherwise, throw an exception.
     /// </summary>
     public void MoveDown()
     {
-        // FILL IN CODE
+        // Index 3 represents the Down direction
+        // Reminder: moving down usually increases Y
+        if (_mazeMap.ContainsKey((_currX, _currY)) && _mazeMap[(_currX, _currY)][3])
+        {
+            _currY++;
+        }
+        else
+        {
+            throw new InvalidOperationException("Can't go that way!");
+        }
     }
 
     public string GetStatus()
