754360a441e296d12c35ec0463fd299b37b08479
MaCK34 <prom23hugomarcelo.sa@gmail.com>
2026-02-11 19:27:52 -0500

    Binary string generator that uses recursion to replace wildcard characters (*) with all possible combinations of '0' and '1'.

--- week05/code/Recursion.cs
+++ week05/code/Recursion.cs
@@ -140,7 +140,15 @@ public static class Recursion
     /// </summary>
     public static void WildcardBinary(string pattern, List<string> results)
     {
-        // TODO Start Problem 4
+        int index = pattern.IndexOf('*');
+        if (index == -1)
+        {
+            results.Add(pattern);
+            return;
+        }
+        // Create 2 versions of the pattern and recurse
+        WildcardBinary(pattern.Substring(0, index) + "0" + pattern.Substring(index + 1), results);
+        WildcardBinary(pattern.Substring(0, index) + "1" + pattern.Substring(index + 1), results);
     }
 
     /// <summary>
